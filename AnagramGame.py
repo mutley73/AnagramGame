a62a8b0757a0bea290e387c24003d1374901709d
mutley73 <65181942+mutley73@users.noreply.github.com>
2020-06-04 16:44:43 +0100

    Add files via upload

--- AnagramGame.py
+++ AnagramGame.py
@@ -1,29 +1,31 @@
 import random
 
 subject = ''
-while subject != '1' and subject != '2':
-    subject = input('\nWould you like birds or countries?\nType 1 for birds or 2 for countries: ')
+while subject != '1' and subject != '2' and subject != '3':
+    subject = input('\nWould you like birds, countries or colours?\nType 1 for birds, 2 for countries or 3 for colours: ')
 if subject == '1':
     filename = 'birds.txt'
 elif subject == '2':
     filename = 'countries.txt'
+else:
+    filename = 'colours.txt'
 
 with open(filename,'r') as f:
-    wordlist = f.read().splitlines()
+    wl = f.read().splitlines()
 
-word = (random.choice(wordlist))
-l = list(word.upper())
+w = (random.choice(wl))
+l = list(w.upper())
 random.shuffle(l)
 sw = ''.join(l)
 print('\nYou have chosen {}.\nYour letters are {}.'.format(filename[:-4],sw))
 
-guess = input('What is the word? ')
-if guess.upper() == word.upper():
+g = input('What is the word? ')
+if g.upper() == w.upper():
     print('\n' + '-' * 33)
     print('Congratulations, that is correct!')
     print('-' * 33)
 else:
-    lc = len(word)
+    lc = len(w)
     print('\n' + '-' * (20 + lc))
-    print('Wrong! The word is {}.'.format(word.upper()))
+    print('Wrong! The word is {}.'.format(w.upper()))
     print('-' * (20 + lc))
